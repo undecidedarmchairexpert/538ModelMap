f4161e61e7a915541e93f63c7ef7c28b8b2e2143
undecidedarmchairexpert <undecidedarmchairexpert@gmail.com>
2020-08-14 00:07:25 -0400

    Reformat Website for Theme

--- index.md
+++ index.md
@@ -1,4 +1,1 @@
-# Fivethirtyeight 2020 Election Model
-## 2016 Style Graphics
-
 ![Model Probabilities](/model_probability.png)
--- modelmap.py
+++ modelmap.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 county_ev = ["NE-1", "NE-2", "NE-3", "ME-1", "ME-2"]
+
 
 forecast_national = pd.read_csv("https://projects.fivethirtyeight.com/2020-general-data/presidential_national_toplines_2020.csv", parse_dates=["modeldate"])
 forecast_state = pd.read_csv("https://projects.fivethirtyeight.com/2020-general-data/presidential_state_toplines_2020.csv", parse_dates=["modeldate"])
 
+
+# Grab information to make the top overall probability plot
 forecast_national_latest = forecast_national.loc[forecast_national.modeldate == forecast_national.modeldate.max()]
 
 probability_r = round(forecast_national_latest.iloc[0].ecwin_inc*100, 2)
@@ -29,7 +33,6 @@ ax.text(99, "Model", str(probability_r)+"%", size=10, color="white", horizontala
 ax.text(0, 0.95, 'Joe Biden\nDemocrat', size=10, horizontalalignment='left', verticalalignment='center', transform=ax.transAxes)
 ax.text(1, 0.95, 'Donald Trump\nRepublican', size=10, horizontalalignment='right', verticalalignment='center', transform=ax.transAxes)
 
-
 ax.xaxis.set_visible(False)
 ax.yaxis.set_visible(False)
 plt.box(False)
@@ -37,6 +40,7 @@ plt.box(False)
 plt.savefig('model_probability.png', transparent=True)
 
 
+# Grab the information to make the choropleth state-by-state map
 forecast_state_latest = forecast_state.loc[forecast_state.modeldate == forecast_state.modeldate.max()]
 forecast_state_latest_county_ev = forecast_state_latest.loc[forecast_state_latest.state.isin(county_ev)]
 forecast_state_latest_state_ev = forecast_state_latest.loc[forecast_state_latest.state.isin(county_ev)]
