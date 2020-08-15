b59b45e08e9260964de0d8443a95fdf165527994
undecidedarmchairexpert <undecidedarmchairexpert@gmail.com>
2020-08-15 01:30:06 -0400

    Add Choropleth Map

--- index.md
+++ index.md
@@ -1,1 +1,12 @@
+# Who will win the presidency?
+---
+## Chance of winning
 ![Model Probabilities](/model_probability.png)
+
+![Choropleth Map](/choropleth_map.png)
+
+##### Key
+![Map Key](/map_key.png)
+
+
+# [Click here for an interactive version of the map](choropleth_map.html)
--- modelmap.py
+++ modelmap.py
@@ -1,5 +1,7 @@
+import us
 import pandas as pd
 import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 
 
 county_ev = ["NE-1", "NE-2", "NE-3", "ME-1", "ME-2"]
@@ -43,4 +45,46 @@ plt.savefig('model_probability.png', transparent=True)
 # Grab the information to make the choropleth state-by-state map
 forecast_state_latest = forecast_state.loc[forecast_state.modeldate == forecast_state.modeldate.max()]
 forecast_state_latest_county_ev = forecast_state_latest.loc[forecast_state_latest.state.isin(county_ev)]
-forecast_state_latest_state_ev = forecast_state_latest.loc[forecast_state_latest.state.isin(county_ev)]
+forecast_state_latest_state_ev = forecast_state_latest.loc[~forecast_state_latest.state.isin(county_ev)].reset_index(drop=True)
+
+state_abbr = list()
+for state in forecast_state_latest_state_ev.state:
+    state_abbr.append(us.states.lookup(state).abbr)
+forecast_state_latest_state_ev["state_abbr"] = state_abbr
+
+for col in forecast_state_latest_state_ev.columns:
+    forecast_state_latest_state_ev[col] = forecast_state_latest_state_ev[col].astype(str)
+
+forecast_state_latest_state_ev['winstate_num_chal'] = forecast_state_latest_state_ev['winstate_chal'].apply(lambda x: float(x))
+forecast_state_latest_state_ev['winstate_num_inc'] = forecast_state_latest_state_ev['winstate_inc'].apply(lambda x: float(x))
+
+forecast_state_latest_state_ev["winperc_inc"] = round(forecast_state_latest_state_ev['winstate_num_inc'].multiply(100), 2).apply(lambda x: str(x) + "%")
+forecast_state_latest_state_ev["winperc_chal"] = round(forecast_state_latest_state_ev['winstate_num_chal'].multiply(100), 2).apply(lambda x: str(x) + "%")
+
+forecast_state_latest_state_ev['text'] = forecast_state_latest_state_ev['state'] + '<br>' + \
+    'Donald Trump ' + forecast_state_latest_state_ev["winperc_inc"] + '<br>' + \
+    'Joe Biden ' + forecast_state_latest_state_ev["winperc_chal"] + '<br>'
+
+fig = go.Figure(data=go.Choropleth(
+    locations=forecast_state_latest_state_ev['state_abbr'],
+    z=forecast_state_latest_state_ev['winstate_inc'],
+    locationmode='USA-states',
+    colorscale=[[0, "rgb(0, 174, 243)"],
+                [0.5, "rgb(245,245,245)"],
+                [1, "rgb(232, 27, 35)"]],
+    autocolorscale=False,
+    text=forecast_state_latest_state_ev['text'], # hover text
+    marker_line_color='grey',
+    showscale=False,
+))
+
+fig.update_layout(
+    geo = dict(
+        scope='usa',
+        projection=go.layout.geo.Projection(type = 'albers usa'),
+        showlakes=False,
+    ),
+)
+
+fig.write_html("choropleth_map.html")
+fig.write_image("choropleth_map.png")
